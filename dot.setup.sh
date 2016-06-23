c64502d65bbe4d5511ba4643ca8b0576a45c038f
Lukas Nellen <lukas@nucleares.unam.mx>
2016-06-23 14:33:42 +0200

    Import files. Personal setup!

---
+++ dot.setup.sh
@@ -0,0 +1,30 @@
+# Simple setup script invoker for bash with completion
+# Author: Lukas Nellen
+#
+# Setup scripts are in $HOME/setup.d
+# They get sourced to modify the current shell's environment
+#
+# Special case if $PYENV set to point to a directory with python virtual envs:
+#   setup py "the-env"
+# will activate the python environment "the-env" in ${PYENV}
+#
+# Configure automatic name completion 
+
+_setup_complete () {
+    local cur="$2"
+    if [ ${COMP_CWORD} -gt 1 -a -f "${HOME}/.setup.d/${COMP_WORDS[1]}.compgen" ]
+    then
+	COMPREPLY=( $(bash "${HOME}/.setup.d/${COMP_WORDS[1]}.compgen" "${cur}" ) )
+    else
+	COMPREPLY=( $(builtin cd $HOME/.setup.d && \
+		   compgen -f -X "*[~.]*" -- "${cur}" ) )
+    fi
+}
+
+function setup () {
+    script=$1
+    shift
+    source $HOME/.setup.d/${script} "$@"
+}
+
+complete -o nospace -F _setup_complete setup
